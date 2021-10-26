e4bc0a2efdd0bd13efbc26f69a0482c0d6323127
gnii <chochain@yahoo.com>
2021-10-25 18:59:59 -0700

    ~/tests and Hello.java and nanojvm/Forth.java

---
+++ tests/Hello.java
@@ -0,0 +1,10 @@
+import nanojvm.Forth;
+
+class Hello
+{
+    public static void main()
+    {
+        Forth.words();
+        Forth.clock();
+    }
+}
---
+++ tests/nanojvm/Forth.java
@@ -0,0 +1,8 @@
+package nanojvm;
+
+public class Forth {
+    public static void words() {}
+    public static void here()  {}
+    public static void clock() {}
+    public static void bye()   {}
+}
