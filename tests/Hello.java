7805e7ea604af14143403d8b64209a275b9f1526
gnii <chochain@yahoo.com>
2021-10-27 19:23:53 -0700

    ~/tests/Hello add variables

--- tests/Hello.java
+++ tests/Hello.java
@@ -2,8 +2,14 @@ import nanojvm.Forth;
 
 class Hello
 {
+    static int cls_v0;
+    int inst_v0;
+    int inst_v1;
+    
     public static void main()
     {
+        int main_v0;
+        
         Forth.words();
         Forth.clock();
     }
