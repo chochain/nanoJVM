dc9e9c1f29439c1490e27278b1a1c74f01e30a4b
gnii <chochain@yahoo.com>
2021-10-29 18:15:19 -0700

    ~/tests/nanojvm/Forth add ss,dump,tick

--- tests/nanojvm/Forth.java
+++ tests/nanojvm/Forth.java
@@ -2,7 +2,10 @@ package nanojvm;
 
 public class Forth {
     public static void words() {}
-    public static void here()  {}
-    public static void clock() {}
+    public static void ss()    {}
     public static void bye()   {}
+    public static int  here()  { return 0; }
+    public static int  clock() { return 0; }
+    public static void dump(int a, int sz) {}
+    public static int  tick(String s)  { return 0; }
 }
