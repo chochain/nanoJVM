e3c3a6702ff1cc1b7fc97bb8e466041430d6d5a8
gnii <chochain@yahoo.com>
2021-10-31 13:44:01 -0700

    ~/tests add Forth.delay

--- tests/InstVar.java
+++ tests/InstVar.java
@@ -13,6 +13,7 @@ class InstVar
         
         h.iv1 = Forth.clock();          // b8 00 04 b5 00 05
         cv0   = Forth.here();           // b8 00 06 b3 00 07
+        Forth.delay(500);
         h.iv1 -= Forth.clock();         // 2b 59 b4 00 05 b8 00 04 64 b5 00 05
         cv0   = h.iv0 = -h.iv1;         // 2b 2b b4 00 05 74 5a b5 00 08 b3 00 07
     }                                   // b1
--- tests/nanojvm/Forth.java
+++ tests/nanojvm/Forth.java
@@ -6,6 +6,7 @@ public class Forth {
     public static void bye()   {}
     public static int  here()  { return 0; }
     public static int  clock() { return 0; }
+    public static void delay(int t)        {}
     public static void dump(int a, int sz) {}
-    public static int  tick(String s)  { return 0; }
+    public static int  tick(String s)      { return 0; }   // TODO
 }
