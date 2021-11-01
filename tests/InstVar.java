b8598a6ff0d33db556366a56629565d4f70bf06b
gnii <chochain@yahoo.com>
2021-10-31 17:29:28 -0700

    ~/tests/InstVar add opcode dump; compiled with -g:none

--- tests/InstVar.java
+++ tests/InstVar.java
@@ -2,9 +2,9 @@ import nanojvm.Forth;
 
 class InstVar
 {
-    static int cv0;                // global[05]
-    int iv0;
-    int iv1;
+    static int cv0;                     // [07]
+    int iv0;                            // [09]
+    int iv1;                            // [05]
     
     public static void main()
     {
@@ -13,8 +13,9 @@ class InstVar
         
         h.iv1 = Forth.clock();          // b8 00 04 b5 00 05
         cv0   = Forth.here();           // b8 00 06 b3 00 07
-        Forth.delay(500);
+        Forth.delay(500);               // 11 01 f4 b8 00 08
         h.iv1 -= Forth.clock();         // 2b 59 b4 00 05 b8 00 04 64 b5 00 05
-        cv0   = h.iv0 = -h.iv1;         // 2b 2b b4 00 05 74 5a b5 00 08 b3 00 07
+        v0    = h.iv0 = -h.iv1;         // 2b 2b b4 00 05 74 5a b5 00 09 3b
+        cv0   = v0;                     // 1a b3 00 07
     }                                   // b1
 }
