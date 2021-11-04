8be2624b94a03f35a1401935ec09b719dca21875
gnii <chochain@yahoo.com>
2021-11-04 09:40:22 -0700

    ~/tests/nanojvm/Forth add decimal/hex

--- tests/nanojvm/Forth.java
+++ tests/nanojvm/Forth.java
@@ -1,12 +1,14 @@
 package nanojvm;
 
 public class Forth {
-    public static void words() {}
-    public static void ss()    {}
-    public static void bye()   {}
-    public static int  here()  { return 0; }
-    public static int  clock() { return 0; }
-    public static void delay(int t)        {}
+    public static void decimal() {}
+    public static void hex()     {}
+    public static int  here()    { return 0; }
+    public static void words()   {}
+    public static void ss()      {}
     public static void dump(int a, int sz) {}
-    public static int  tick(String s)      { return 0; }   // TODO
+    public static int  tick(String s)      { return 0; }  // TODO
+    public static int  clock()             { return 0; }
+    public static void delay(int t)        {}
+    public static void bye()     {}
 }
