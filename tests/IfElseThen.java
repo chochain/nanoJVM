cf03a05fead1b3c8892165ba3f1d47b337cae7c5
gnii <chochain@yahoo.com>
2021-11-05 08:29:59 -0700

    ~/tests/IfElseThen add opcode comment

--- tests/IfElseThen.java
+++ tests/IfElseThen.java
@@ -5,16 +5,16 @@ class IfElseThen
     public static void main()
     {
         int v0;                   // xs[0]
-        int v1 = 0;
+        int v1 = 0;               // 18 03 3c
         int v2;
         int v3;
-        int v4 = 0x7fff;
+        int v4 = 0x7fff;          // 11 7f ff 36 04 15 04
         
-        if (v4 > v1) {
-            Forth.clock();
+        if (v4 > v1) {            // 1b a4 00 0a
+            Forth.clock();        // b8 00 02 57 a7 00 06
         }
         else {
-            Forth.ss();
+            Forth.ss();           // b8 00 03
         }
-    }                                   // b1
+    }                             // b1
 }
