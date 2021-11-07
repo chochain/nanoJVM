d3c57aff6f5c1e5732df766195c503cf1d6a9274
gnii <chochain@yahoo.com>
2021-11-07 13:15:35 -0800

    ~/src/esp32.cpp add debugging code

--- src/esp32.cpp
+++ src/esp32.cpp
@@ -8,10 +8,10 @@
 #define analogWrite(c,v,mx) ledcWrite((c),(8191/mx)*min((int)(v),mx))
 #endif // ESP32
 static Method _esp32[] = {
-#if ARDUINO && ESP32
     ///
     /// @definegroup ESP32 supporting functions
     /// @{
+#if ARDUINO && ESP32
     CODE("pinMode",      DU p = POP; pinMode(p, POP)),
     CODE("digitalRead",  PUSH(digitalRead(POP))),
     CODE("digitalWrite", DU p = POP; digitalWrite(p, POP)),
@@ -20,8 +20,18 @@ static Method _esp32[] = {
     CODE("ledcAttachPin",DU p  = POP; ledcAttachPin(p, POP)),
     CODE("ledcSetup",    DU ch = POP; DU freq=POP; ledcSetup(ch, freq, POP)),
     CODE("ledcWriteTone",DU ch = POP; ledcWriteTone(ch, POP)),
-    /// @}
+#else
+    /// for debugging
+    CODE("pinMode",      DU p = POP; DU m = POP),
+    CODE("digitalRead",  DU p = POP; PUSH(t.IP)),
+    CODE("digitalWrite", DU p = POP; DU v = POP),
+    CODE("analogRead",   DU p = POP; PUSH(t.IP)),
+    CODE("analogWrite",  DU p  = POP; DU v = POP),
+    CODE("ledcAttachPin",DU p  = POP; DU v = POP),
+    CODE("ledcSetup",    DU ch = POP; DU freq=POP; DU v = POP),
+    CODE("ledcWriteTone",DU ch = POP; DU v = POP)
 #endif // ARDUINO
+    /// @}
 };
 ///
 /// ESP32 built-in word in ROM, use extern by main program
