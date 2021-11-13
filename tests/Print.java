0b76d96a24b10cd6e82c3102f915cdf9c856a454
gnii <chochain@yahoo.com>
2021-11-12 19:04:22 -0800

    ~/tests add Print case

---
+++ tests/Print.java
@@ -0,0 +1,10 @@
+import ej32.Forth;
+
+class Print
+{
+    public static void main() {
+        System.out.print("test ");
+        System.out.print(123);
+        System.out.println(" ok!");
+    }                                   // b1
+}
