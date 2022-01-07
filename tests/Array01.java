3bff760500348eebf67941da775fef1dd11fbb4c
gnii <chochain@yahoo.com>
2022-01-07 17:32:53 +0800

    ~/tests/Array0x adjust numbers range

--- tests/Array01.java
+++ tests/Array01.java
@@ -2,7 +2,7 @@ class Array01
 {
     public static void main(String[] av) {     // 2a b7 00 01 b1
         int a[] =                      // 06 bc 0a
-            {1,2,3};                   // 59 03 04 4f 59 04 05 4f 59 05 06 4f
+            {2,12,22};                 // 59 03 05 4f 59 04 10 0c 4f 59 05 10 16 4f
 
         System.out.println(a.length);  // 4c b2 00 02 2b be b6 00 03 b2 00 02
         System.out.println(a[0]);      // 2b be b6 00 03 b2 00 02
--- tests/Array02.java
+++ tests/Array02.java
@@ -1,16 +1,16 @@
 class Array02
 {
     public static void main(String[] av) {     // 2a b7 00 01 b1
-        int a[][] = {                  // 05 bd 00 02 59
-            {1,2,3},                   // 59 03 06 bc 0a 59 03 04 06 6 bc 0a
-            {4,5,6}
+        int a[][] = {                  // 05 bd 00 02
+            {2,12,22},                 // 59 03 06 bc 0a 59 05 10 0c 4f 59 05 10 16 4f 53
+            {44,55,66}                 // 59 03 10 2c 4f 59 03 10 37 4f 59 04 10 42 4f 53
         };
 
-        System.out.println(a.length);  // 4c b2 00 02 2b be b6 00 03 b2 00 02
-        for (int i=0; i<2; i++) {
+        System.out.println(a.length);  // 4c b2 00 03 2b be b6 00 04
+        for (int i=0; i<2; i++) {      // 03 3d 1c 05 a2 00 21 03 3e
             for (int j=0; j<3; j++) {
-                System.out.println(a[i][j]);      // 2b be b6 00 03 b2 00 02
-            }
-        }
+                System.out.println(a[i][j]);  // 2b 1c 32 1d 2e b6 00 04
+            }                          // 84 03 01 a7 ff ed 
+        }                              // 84 02 01 a7 ff e0
     }                                  // b1
 }
